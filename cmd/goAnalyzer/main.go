785744bf9788735c3910a6ed44ebe356a7930e22
Barend Scholtus <barend.scholtus@gmail.com>
2025-06-14 01:08:26 +0700

    Added basic counters

--- cmd/goAnalyzer/main.go
+++ cmd/goAnalyzer/main.go
@@ -1,14 +1,69 @@
+// Package main provides a CLI application to process Apache log files.
 package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"sort"
+	"strconv"
 
-	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v3"
 )
+
+type Stats struct {
+	hits           int            // Any request made to the server is a 'hit'
+	files          int            // Responses sent back to the requesting client (HTML page, image...)
+	pages          int            // Any HTML document—or anything that generates an HTML document—would
+	sites          int            // Requests originating from 'site', referenced by a name or IP address
+	visits         int            // Number of new or timed out IP addresses
+	bytes          int            // Amount of data that was sent out by the server
+	hitsByMethod   map[string]int // Hits by HTTP method
+	hitsByProtocol map[string]int // Hits by HTTP protocol
+	hitsByRespCode map[string]int // Hits by HTTP response code
+}
+
+func (s Stats) String() string {
+	output := fmt.Sprintf("Stats:\n"+
+		"  Hits:   %d\n"+
+		"  Files:  %d\n"+
+		"  Pages:  %d\n"+
+		"  Sites:  %d\n"+
+		"  Visits: %d\n"+
+		"  Bytes:  %d bytes\n",
+		s.hits, s.files, s.pages, s.sites, s.visits, s.bytes)
+
+	output += fmt.Sprintln("  Hits by HTTP method")
+	for _, method := range GetSortedKeys(&s.hitsByMethod) {
+		output += fmt.Sprintf("    %-7s: %d\n", method, s.hitsByMethod[method])
+	}
+
+	output += fmt.Sprintln("  Hits by HTTP protocol")
+	for _, protocol := range GetSortedKeys(&s.hitsByProtocol) {
+		output += fmt.Sprintf("    %-8s: %d\n", protocol, s.hitsByProtocol[protocol])
+	}
+
+	output += fmt.Sprintln("  Hits by HTTP response code")
+	for _, retCode := range GetSortedKeys(&s.hitsByRespCode) {
+		output += fmt.Sprintf("    %s: %d\n", retCode, s.hitsByRespCode[retCode])
+	}
+
+	return output
+}
+
+func GetSortedKeys(m *map[string]int) []string {
+	keys := make([]string, 0, len(*m))
+	for k := range *m {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	return keys
+}
 
 func processLog(fileName string) {
 	// Open the file
@@ -19,47 +74,80 @@ func processLog(fileName string) {
 	}
 	defer file.Close()
 
-	var count int = 0
+	lineNr := 0
+	stats := Stats{}
+	stats.hitsByMethod = make(map[string]int)
+	stats.hitsByProtocol = make(map[string]int)
+	stats.hitsByRespCode = make(map[string]int)
 
-	/* var (
-		host       string
-		dash       string
-		user       string
-		timestamp  string
-		request    string
-		statusCode int
-		bytes      int
-		referrer   string
-		useragent  string
-	) */
+	// How to recognise a Page
+	//                         1     2     3       4        5        6        7                  8     9           10      11
+	re := regexp.MustCompile(`^(\S+) (\S+) (\S+) \[(.*?)\] "([A-Z]+) (.*?)(?: (HTTP\/[0-9.]+))?" (\d+) (-|\d+)(?: "(.*?)" "(.*?)")?$`)
+	re2 := regexp.MustCompile(`(?i)\.(htm|html|htmlx|dhtml|phtml|php3|php4|php|asp|aspx|cfm|cfml|jsp|jspx|shtml|stm|cgi|pl|py|ejs|erb|haml|handlebars|mustache|twig|jade|pug|dust|liquid|md|markdown|html\.erb|rst|adoc|hbs)`)
 
-	re := regexp.MustCompile(`^(\S+) (\S+) (\S+) \[(.*?)\] "(.*?)" (\d+) (\d+) "(.*?)" "(.*?)"$`)
+	PageURLRegExes := []string{"^/$", "^/blog", "^/articles", "^/projects"}
+	var compiledRegexes []*regexp.Regexp
+	for _, regexStr := range PageURLRegExes {
+		compiledRegex, err := regexp.Compile(regexStr)
+		if err != nil {
+			fmt.Println("Invalid regex:", regexStr)
+			continue
+		}
+		compiledRegexes = append(compiledRegexes, compiledRegex)
+	}
 
 	// Read the file line by line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		lineNr++
+
 		line := scanner.Text()
-		if line != "" {
-			match := re.FindStringSubmatch(line)
+		if line == "" {
+			fmt.Println("Empty line", lineNr)
+			continue
+		}
 
-			if match == nil {
-				fmt.Println("No match")
-				return
+		match := re.FindStringSubmatch(line)
+		if match == nil {
+			fmt.Println("Invalid line", lineNr)
+			continue
+		}
+
+		// any valid log entry is a hit
+		stats.hits++
+
+		// count hits by HTTP method, protocol, response code
+		stats.hitsByMethod[match[5]]++
+		if match[7] == "" {
+			match[7] = "Unknown"
+		}
+		stats.hitsByProtocol[match[7]]++
+		stats.hitsByRespCode[match[8]]++
+
+		// any GET request sends something back
+		if match[5] == "GET" {
+			stats.files++
+
+			// any of the predefined file extensions is a page
+			if re2.FindStringIndex(match[6]) != nil {
+				stats.pages++
+			} else {
+				// any of the predefined Page URLs is also a page
+				for _, re := range compiledRegexes {
+					if re.FindStringIndex(match[6]) != nil {
+						stats.pages++
+					}
+				}
 			}
+		}
 
-			count++
+		stats.sites = 0
+		stats.visits = 0
 
-			fmt.Printf("Host: %s\n", match[1])
-			fmt.Printf("Dash: %s\n", match[2])
-			fmt.Printf("User: %s\n", match[3])
-			fmt.Printf("Timestamp: %s\n", match[4])
-			fmt.Printf("Request: %s\n", match[5])
-			fmt.Printf("Status Code: %s\n", match[6])
-			fmt.Printf("Bytes: %s\n", match[7])
-			fmt.Printf("Referrer: %s\n", match[8])
-			fmt.Printf("User-Agent: %s\n", match[9])
-
-			return
+		// the number of bytes sent back
+		bytes, err := strconv.Atoi(match[9])
+		if err == nil {
+			stats.bytes += bytes
 		}
 	}
 
@@ -68,25 +156,28 @@ func processLog(fileName string) {
 		fmt.Printf("Error reading file: %v\n", err)
 	}
 
+	// print
+	fmt.Println(stats)
 }
 
 func main() {
-	app := &cli.App{
+	cmd := &cli.Command{
 		Name:  "file-cli",
 		Usage: "A simple CLI that takes a file name as an argument",
-		Action: func(c *cli.Context) error {
-			if c.NArg() != 1 {
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			if cmd.NArg() != 1 {
 				return fmt.Errorf("please provide exactly one file name")
 			}
 
-			fileName := c.Args().Get(0)
+			fileName := cmd.Args().Get(0)
 			processLog(fileName)
 
 			return nil
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
 		log.Fatal(err)
 	}
+
 }

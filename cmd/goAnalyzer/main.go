6e69f96de3b87e07f1e071cba8ffe8bd3cf78322
Barend Scholtus <barend.scholtus@gmail.com>
2025-06-15 04:35:06 +0700

    Added sites and vists stats. Refactored.

--- cmd/goAnalyzer/main.go
+++ cmd/goAnalyzer/main.go
@@ -10,23 +10,35 @@ import (
 	"regexp"
 	"sort"
 	"strconv"
+	"time"
 
 	"github.com/urfave/cli/v3"
 )
 
 type Stats struct {
-	hits           int            // Any request made to the server is a 'hit'
-	files          int            // Responses sent back to the requesting client (HTML page, image...)
-	pages          int            // Any HTML document—or anything that generates an HTML document—would
-	sites          int            // Requests originating from 'site', referenced by a name or IP address
-	visits         int            // Number of new or timed out IP addresses
-	bytes          int            // Amount of data that was sent out by the server
-	hitsByMethod   map[string]int // Hits by HTTP method
-	hitsByProtocol map[string]int // Hits by HTTP protocol
-	hitsByRespCode map[string]int // Hits by HTTP response code
+	hits           int                  // Any request made to the server is a 'hit'
+	files          int                  // Responses sent back to the requesting client (HTML page, image...)
+	pages          int                  // Any HTML document—or anything that generates an HTML document—would
+	siteNames      map[string]int       // Each request made to the server comes from a unique 'site'
+	lastVisit      map[string]time.Time // Last hit' timestamp per site
+	visits         int                  // Number of new or timed out IP addresses
+	bytes          int                  // Amount of data that was sent out by the server
+	hitsByMethod   map[string]int       // Hits by HTTP method
+	hitsByProtocol map[string]int       // Hits by HTTP protocol
+	hitsByRespCode map[string]int       // Hits by HTTP response code
 }
 
-func (s Stats) String() string {
+func NewStats() *Stats {
+	stats := Stats{}
+	stats.siteNames = make(map[string]int)
+	stats.lastVisit = make(map[string]time.Time)
+	stats.hitsByMethod = make(map[string]int)
+	stats.hitsByProtocol = make(map[string]int)
+	stats.hitsByRespCode = make(map[string]int)
+	return &stats
+}
+
+func (s *Stats) String() string {
 	output := fmt.Sprintf("Stats:\n"+
 		"  Hits:   %d\n"+
 		"  Files:  %d\n"+
@@ -34,7 +46,7 @@ func (s Stats) String() string {
 		"  Sites:  %d\n"+
 		"  Visits: %d\n"+
 		"  Bytes:  %d bytes\n",
-		s.hits, s.files, s.pages, s.sites, s.visits, s.bytes)
+		s.hits, s.files, s.pages, len(s.siteNames), s.visits, s.bytes)
 
 	output += fmt.Sprintln("  Hits by HTTP method")
 	for _, method := range GetSortedKeys(&s.hitsByMethod) {
@@ -75,10 +87,7 @@ func processLog(fileName string) {
 	defer file.Close()
 
 	lineNr := 0
-	stats := Stats{}
-	stats.hitsByMethod = make(map[string]int)
-	stats.hitsByProtocol = make(map[string]int)
-	stats.hitsByRespCode = make(map[string]int)
+	stats := NewStats()
 
 	// How to recognise a Page
 	//                         1     2     3       4        5        6        7                  8     9           10      11
@@ -96,18 +105,15 @@ func processLog(fileName string) {
 		compiledRegexes = append(compiledRegexes, compiledRegex)
 	}
 
+	layout := "02/Jan/2006:15:04:05 -0700"
+	visitTimeout := 600 * time.Second
+
 	// Read the file line by line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lineNr++
 
-		line := scanner.Text()
-		if line == "" {
-			fmt.Println("Empty line", lineNr)
-			continue
-		}
-
-		match := re.FindStringSubmatch(line)
+		match := re.FindStringSubmatch(scanner.Text())
 		if match == nil {
 			fmt.Println("Invalid line", lineNr)
 			continue
@@ -116,39 +122,50 @@ func processLog(fileName string) {
 		// any valid log entry is a hit
 		stats.hits++
 
-		// count hits by HTTP method, protocol, response code
-		stats.hitsByMethod[match[5]]++
-		if match[7] == "" {
-			match[7] = "Unknown"
-		}
-		stats.hitsByProtocol[match[7]]++
-		stats.hitsByRespCode[match[8]]++
-
-		// any GET request sends something back
-		if match[5] == "GET" {
+		// "Found" increases files
+		if match[8] == "200" {
 			stats.files++
+		}
 
-			// any of the predefined file extensions is a page
-			if re2.FindStringIndex(match[6]) != nil {
-				stats.pages++
-			} else {
-				// any of the predefined Page URLs is also a page
-				for _, re := range compiledRegexes {
-					if re.FindStringIndex(match[6]) != nil {
-						stats.pages++
-					}
+		// any of the predefined file extensions is a page
+		if re2.FindStringIndex(match[6]) != nil {
+			stats.pages++
+		} else {
+			// any of the predefined Page URLs is also a page
+			for _, re := range compiledRegexes {
+				if re.FindStringIndex(match[6]) != nil {
+					stats.pages++
 				}
 			}
 		}
 
-		stats.sites = 0
-		stats.visits = 0
+		// count visits
+		stats.siteNames[match[1]]++
+
+		t, err := time.Parse(layout, match[4])
+		if err != nil {
+			fmt.Println("Error parsing time:", err)
+			return
+		}
+		if t.Sub(stats.lastVisit[match[1]]) > visitTimeout {
+			stats.visits++
+		}
+		stats.lastVisit[match[1]] = t
 
 		// the number of bytes sent back
 		bytes, err := strconv.Atoi(match[9])
 		if err == nil {
 			stats.bytes += bytes
 		}
+
+		// count hits by HTTP method, protocol, response code
+		stats.hitsByMethod[match[5]]++
+		if match[7] == "" {
+			stats.hitsByProtocol["Unknown"]++
+		} else {
+			stats.hitsByProtocol[match[7]]++
+		}
+		stats.hitsByRespCode[match[8]]++
 	}
 
 	// Check for errors
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/rbscholtus/go-webalizer
 
 go 1.24.4
 
-require github.com/urfave/cli/v3 v3.3.3
+require github.com/urfave/cli/v3 v3.3.8
